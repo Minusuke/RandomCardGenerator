11e8bf89c147622c97331dd496da4f79afe053a7
Minu <122645470+Minusuke@users.noreply.github.com>
2023-06-08 20:53:39 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./style.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function randomNumber() {
+    let numbers = [
+      "A",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumber = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumber];
+  }
+  function randomSuit() {
+    let suits = ["♦", "♥", "♠", "♣"];
+    let indexSuit = Math.floor(Math.random() * suits.length);
+
+    if (suits[indexSuit] === "♦" || suits[indexSuit] === "♥") {
+      suits[indexSuit] = `<span style="color: red;">${suits[indexSuit]}</span>`;
+    }
+
+    return suits[indexSuit];
+  }
+  let finalNumber = randomNumber();
+  document.querySelector(".number").innerHTML = finalNumber;
+
+  let palos = randomSuit();
+  document.querySelector(".card-top").innerHTML = palos;
+  document.querySelector(".card-bottom").innerHTML = palos;
+
+  var boton = document.getElementById("miBoton");
 };
